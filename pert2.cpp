e70d6d294e90b94d54a0049c0f6e6e53c7c4e44d
restiananda <restiananda260@gmail.com>
2025-12-19 19:11:37 +0700

    Add basic C++ program with personal introduction

---
+++ pert2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string nama = "--Fajar Resti Ananda--";
+  string NPM = "--2517051063--";
+  
+  cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+  cout << "Nama saya adalah " << Fajar Resti Ananda << endl; 
+  cout << "NPM saya adalah " << 2517051063 << endl << endl;
+  
+  cout << "Unila Indah" << endl;
+  cout << "Be Strong" << endl;
+} 
